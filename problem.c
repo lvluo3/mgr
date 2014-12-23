b161d5c80c5658ca56d11e8be7b27a78b5175fa9
xxx <xxx@X.(none)>
2014-12-23 16:18:25 +0800

    p

---
+++ problem.c
@@ -0,0 +1,17 @@
+1.网络异步io，libevent
+2.写osip
+3.进程关系
+4.锁的实现
+5.信号量为啥能跨进程？是怎么实现的？
+6.connect 超时
+7.epoll 优于select的原因
+8.listen ，block参数的意义
+9.端口范围
+10.位图，红黑树
+11.sip的呼叫过程
+12.详解sip状态机
+13.udp，connect 的作用
+14.shutdown 与close区别
+15.mmap, 分配内存
+16.1xx 到6xx 大概意思
+。。。
